5530b9233d03c29acea703684d7475bf94fd3845
KateLis84 <emma12022002@gmail.com>
2023-08-04 21:19:51 +0300

    add some changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,17 @@ import Auth from "./auth/Auth";
 import Header from "./Modules/Header/Header.js"
 import MainPage from "./Pages/MainPage/MainPage.js";
 
-import PopupAuth from "./Modules/PopupAuth/PopupAuth.js";
-
 function App() {
   return (
     <div className="App">
       <Header/>
       <Router>
         <Routes>
-          <Route exact path="/auth" element={<MainPage/>}/>
+          <Route exact path="/auth" element={<Auth/>}/>
+          <Route exact path="/" element={<MainPage/>}/>
           <Route path="*" element={<Navigate to={'/auth'} replace/>}/>
         </Routes>
       </Router>
-      {/* <PopupAuth/> */}
     </div>
   );
 }
