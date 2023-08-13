a428559df0b6cd602604cd94e409abfa3d8adec5
Denys Serhieiev <denys.serhieiev@easternpeak.com>
2023-08-14 01:43:08 +0300

    update fakeLogin

--- src/Store/Thunks/Auth/fakelogin.jsx
+++ src/Store/Thunks/Auth/fakelogin.jsx
@@ -34,8 +34,11 @@ const fakelogin = createAsyncThunk('auth/login2', async (payload) => {
     console.warn('Error on login', response); //todo make error better
   }
 
-  // responseParsed.user = payload;
-  responseParsed.user = user;
+  responseParsed.user = {
+    name: responseParsed.Name,
+    avatar: responseParsed.Avatar,
+    role: responseParsed.Role,
+  };
 
   return responseParsed;
 });
