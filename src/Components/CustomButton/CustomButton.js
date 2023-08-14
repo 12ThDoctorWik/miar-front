deedcfa66408278816381e324862203762bed6e0
Denys Serhieiev <denys.serhieiev@easternpeak.com>
2023-08-14 08:30:26 +0300

    Add Custom Button. update CreateGame page

--- src/Components/CustomButton/CustomButton.js
+++ src/Components/CustomButton/CustomButton.js
@@ -4,11 +4,12 @@ import { Button } from '@mui/material';
 import { Link } from "react-router-dom";
 import AddIcon from '@mui/icons-material/Add';
 
-export default function CustomButton({type, to}) {
+export default function CustomButton({type, to, text=null, showIcon = true}) {
 
   return(
     <Button className={"custom_button custom_button"+type} component={Link}  to={to}>
-      <AddIcon></AddIcon>
+      {showIcon && <AddIcon></AddIcon>}
+      {text}
     </Button>
   )
 }
--- src/Pages/CreateGame/CreateGame.js
+++ src/Pages/CreateGame/CreateGame.js
@@ -1,42 +1,153 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./CreateGame.scss";
 
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import CustomButton from "../../Components/CustomButton/CustomButton";
 
+// import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
+// import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import moment from "moment";
+import 'moment/locale/uk';
+import { Field, Form } from 'react-final-form';
+
+import { Checkboxes, DateTimePicker, Select, TextField } from 'mui-rff';
+
+const fields = [
+    'name'
+]
+const validate = (values) => {
+  console.log('validate', values);
+  const errors = {};
+  const checks = ['startTime'];
+  checks.forEach((field) => {
+    if (!values[field]) {
+      errors[field] = 'Required';
+    }
+  });
+
+  return errors;
+};
 
 export default function CreateGame() {
+  const [gameDatetime, setGameDatetime] = useState(moment());
 
-  return(
-    <div className="createGame">
-      <div className="createGame__container">
-        <TextField id="outlined-basic" label="Назва гри" variant="outlined" sx={{color: "white"}} />
-        <TextField id="outlined-basic" label="Клуб" variant="outlined" sx={{color: "white"}} />
-        <TextField id="outlined-basic" label="Місто" variant="outlined" sx={{color: "white"}} />
-        <TextField id="outlined-basic" label="Майстер" variant="outlined" sx={{color: "white"}} />
-        <TextField id="outlined-basic" label="Локація" variant="outlined" sx={{color: "white"}} />
-        <TextField id="outlined-basic" label="Ігрова система" variant="outlined" sx={{color: "white"}} />
-        <TextField id="outlined-basic" label="Кількість гравців" variant="outlined" sx={{color: "white"}} />
-        <TextField id="outlined-basic" label="Складність пригоди" variant="outlined" sx={{color: "white"}} />
-        <TextField id="outlined-basic" label="Ціна" variant="outlined" sx={{color: "white"}} />
-        <TextField id="outlined-basic" label="Рівні персонажів" variant="outlined" sx={{color: "white"}} />
+  const onSubmit = async (values) => {
+    console.log('onSubmit', values);
+    if (values.done === undefined) {
+      values.done = false;
+    }
 
-        <input type="date"/>
-        <input type="time"/>
-        {/* <DatePicker label="Дата проведення" /> */}
+    console.log(values);
+    // console.log(event.target[0]);
 
-        <textarea id="w3review" name="w3review" placeholder='Опис'></textarea>
-        <TextField id="outlined-basic" label="Теги" variant="outlined" sx={{color: "white"}} />
-        <div>
-          <input type="file" id="image"/>
-        </div>
-        
+    const newGame = {
+      startTime: moment.utc(gameDatetime).valueOf(),
+      // name: event.target.form
+    }
 
-        <Button variant="contained">Створити гру</Button>
-        <Button variant="filled">Q</Button>
-        
-      </div>
-    </div>
+    console.log('newGameObject', newGame);
+    // const addCallback = () => history.push('/calendar');
+    // dispatch(addTodo(values, addCallback));
+  };
+
+  return (
+      <Form
+          onSubmit={onSubmit}
+          validate={validate}
+          initialValues={{}}
+          render={({ handleSubmit }) => (
+              <form onSubmit={handleSubmit} noValidate>
+                <div className="createGame">
+                  <div className="createGame__container">
+
+                    {/*"name": "string",*/}
+                    {/*"clubId": 0,*/}
+                    {/*"location": "string",*/}
+                    {/*"masterName": "string",*/}
+                    {/*"maxPlayer": 0,*/}
+                    {/*"difficult": 0*/}
+
+                    {/*"locationType": 0, ????*/}
+
+
+                    {/*"description": "string",*/}
+                    {/*"image": "string",*/}
+                    {/*"startTime": "2023-08-13T20:33:47.330Z",*/}
+
+
+                    {/*"minLevel": 0,*/}
+                    {/*"maxLevel": 0,*/}
+                    {/*"visible": 0,*/}
+
+                    {/*"pricePerPlayer": 0,*/}
+
+
+                    {/* --------"system": "string",*/}
+
+                    {/*"tags": [*/}
+                    {/*"string"*/}
+                    {/*],*/}
+
+
+
+
+                    <TextField id="name" name="name" label="Назва гри" variant="outlined"
+                               sx={{ color: "white" }}/>
+                    <TextField id="club" name="club" label="Клуб" variant="outlined" sx={{ color: "white" }} disabled/>
+                    <TextField id="city" name="city" label="Місто" variant="outlined" sx={{ color: "white" }}/>
+                    <TextField id="masterName" name="masterName" label="Майстер" variant="outlined" sx={{ color: "white" }}/>
+                    <TextField id="location" name="location" label="Локація" variant="outlined" sx={{ color: "white" }}/>
+                    <TextField id="system" name="system" label="Ігрова система" variant="outlined" sx={{ color: "white" }}/>
+                    <TextField id="maxPlayer" name="maxPlayer" label="Кількість гравців" variant="outlined" sx={{ color: "white" }}
+                               type={"number"}
+                               InputProps={{
+                                 inputProps: {
+                                   max: 6, min: 1
+                                 }
+                               }}/>
+                    <TextField id="difficult" name="difficult" label="Складність пригоди" variant="outlined" type={"number"}
+                               InputProps={{
+                                 inputProps: {
+                                   max: 5, min: 1
+                                 }
+                               }}
+                               sx={{ color: "white" }}/>
+                    <TextField id="pricePerPlayer" name="pricePerPlayer" label="Ціна" variant="outlined" sx={{ color: "white" }}
+                    type="number"
+                    />
+                    <TextField id="maxLevel" name="maxLevel" label="Рівні персонажів" variant="outlined" sx={{ color: "white" }}
+                               type="number"/>
+
+
+                    {/*<LocalizationProvider dateAdapter={AdapterMoment} adapterLocale={'uk'}>*/}
+                    {/*  <MobileDateTimePicker*/}
+                    {/*      id={"startTime"}*/}
+                    {/*      name={"startTime"}*/}
+                    {/*      type="datetime"*/}
+                    {/*                        ampm={false}*/}
+                    {/*                        format="LLL"*/}
+                    {/*                        value={gameDatetime}*/}
+                    {/*                        onChange={(newValue) => setGameDatetime(newValue)}*/}
+                    {/*  />*/}
+                    {/*</LocalizationProvider>*/}
+
+                    <Field id="description" name="description" component="textarea" placeholder="Опис" />
+                    <TextField id="tags" name="tags" label="Теги" variant="outlined" sx={{ color: "white" }}/>
+                    {/*<div>*/}
+                    {/*  <input type="file" id="image" name="image"/>*/}
+                    {/*</div>*/}
+
+
+                    <Button variant="contained" type="submit">Створити гру</Button>
+                    <CustomButton type={"custom_button custom_button_back"} to={'/calendar'} showIcon={false}
+                                  text={"Назад"}></CustomButton>
+
+
+                  </div>
+                </div>
+              </form>
+          )}
+      />
   )
 }
