898807f6fc04dbd0217c0489328c281af11add40
vhunko <93194789+vhunko@users.noreply.github.com>
2023-08-23 21:27:39 +0300

    QOL

--- src/Pages/GamePage/GamePage.js
+++ src/Pages/GamePage/GamePage.js
@@ -1,10 +1,26 @@
-import { useParams, NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams, NavLink, useNavigate } from 'react-router-dom';
 import { Container, Stack, Box, IconButton, Typography } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import { GameDetails } from '../../Components/GameDetails/GameDetails';
 
 export const GamePage = () => {
   const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const keyDownHandler = event => {
+      if (event.key === 'Escape') {
+        event.preventDefault();
+        navigate('/calendar');
+      }
+    };
+
+    document.addEventListener('keydown', keyDownHandler);
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, []);
 
   return (
     <Container maxWidth="lg" sx={{ paddingTop: 9 }}>
